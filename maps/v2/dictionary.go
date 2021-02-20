c186e68732ddbb1e52b3dcce5527b9b2112b89df
alin <alin.run@foxmail.com>
2021-02-20 21:15:35 +0800

    feat(maps): add maps all code

---
+++ maps/v2/dictionary.go
@@ -0,0 +1,17 @@
+package main
+
+import "errors"
+
+// Dictionary store definitions to words.
+type Dictionary map[string]string
+
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
+// Search find a word in the dictionary
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
+}
---
+++ maps/v5/dictionary.go
@@ -0,0 +1,65 @@
+package main
+
+const (
+	// ErrNotFound means the definition could not be found for the given word
+	ErrNotFound = DictionaryErr("could not find the word you were looking for")
+
+	// ErrWordExists meas you are trying to add a word that is already known
+	ErrWordExists        = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExists = DictionaryErr("cannot update word because it does not exist")
+)
+
+// DictionaryErr are errors that can happen when interacting with the dictionary.
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+// Dictionary store definitions to words.
+type Dictionary map[string]string
+
+// Search find a word in the dictionary
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+// Add inserts a word and definition into the dictionary.
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExists
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
